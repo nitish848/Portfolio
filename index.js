7aa0d058c1df015f18d4c12336969ecc5961ca32
nitish848 <itsnitishrana@gmail.com>
2021-06-13 20:17:13 +0530

    menubar updated

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ let menuicon = document.querySelector(".menuicon");
 let line1 = document.querySelector(".line1");
 let line2 = document.querySelector(".line2");
 let line3 = document.querySelector(".line3");
-const body = document.querySelector("body");
+const menu1 = document.querySelector(".menu1");
 menuicon.addEventListener('click', menuclick);
 function menuclick() {
-    if (line2.style.width == `40px`) {
+    if (line2.style.width === "40px") {
         line2.style.width = `0%`;
         line3.style.width = `100%`;
         line1.style.transform = `rotate(45deg) translate(13px, 6px)`;
         line3.style.transform = `rotate(-45deg) translate(15px, -13px)`;
-        body.style.backgroundColor = `blur`;
+        menu1.style.display = `flex`;
     }
     else {
         line2.style.width = `40px`;
         line3.style.width = `20px`;
         line1.style.transform = `rotate(0deg) translate(0px, 0px)`;
         line3.style.transform = `rotate(0deg) translate(0px, 0px)`;
+        menu1.style.display = `none`;
     }
 }
