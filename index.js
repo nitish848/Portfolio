f2f4aeb8828a765d78a66be5f62aa752f8e0794e
nitish848 <itsnitishrana@gmail.com>
2021-06-13 19:28:57 +0530

    hello

---
+++ index.js
@@ -0,0 +1,20 @@
+let menuicon = document.querySelector(".menuicon");
+let line1 = document.querySelector(".line1");
+let line2 = document.querySelector(".line2");
+let line3 = document.querySelector(".line3");
+
+menuicon.addEventListener('click', menuclick);
+function menuclick() {
+    if (line2.style.width === `40px`) {
+        line2.style.width = `0%`;
+        line3.style.width = `100%`;
+        line1.style.transform = `rotate(45deg) translate(9px, 6px)`;
+        line3.style.transform = `rotate(-45deg) translate(14px, -13px)`;
+    }
+    else {
+        line2.style.width = `40px`;
+        line3.style.width = `20px`;
+        line1.style.transform = `rotate(0deg) translate(0px, 0px)`;
+        line3.style.transform = `rotate(0deg) translate(0px, 0px)`;
+    }
+}
